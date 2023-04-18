89c85dacd8194add909088f3a37ce0e725bc59fb
Dmitry Pershin <dapper1291@gmail.com>
2023-04-19 00:20:21 +0500

    manager tests refactored.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,34 @@
 import random
 from pathlib import Path
+from typing import Generator
 
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def test_dir() -> Path:
     return Path(__file__).parent
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def resource_dir(test_dir) -> Path:
     return test_dir / 'resources'
 
 
 @pytest.fixture(autouse=True)
-def init_random():
+def init_random() -> None:
     random.seed(0)
 
 
 @pytest.fixture(scope='session')
 def sleep_delay(pytestconfig) -> float:
     return pytestconfig.getoption('--sleep-delay', 0.05)
+
+
+@pytest.fixture(scope='session')
+def port_gen() -> Generator[int, None, None]:
+    def gen():
+        for port in range(10000, 65535):
+            yield port
+
+    return gen()
--- tests/contrib/test_socket_manager.py
+++ tests/contrib/test_socket_manager.py
@@ -1,57 +1,75 @@
+import socket
 import socketserver
 import ssl
 import threading
 from ipaddress import IPv4Address
 from pathlib import Path
-from typing import Tuple
+from typing import Generator, Optional, Tuple
 
 import pytest
-import pytest_asyncio.plugin
 
 from generic_connection_pool.contrib.socket import SslSocketConnectionManager, TcpSocketConnectionManager
 from generic_connection_pool.threding import ConnectionPool
 
 
-@pytest.fixture
-async def tcp_server(request: pytest_asyncio.plugin.SubRequest, resource_dir: Path):
-    params = getattr(request, 'param', {})
-    user_ssl = params.get('use_ssl', False)
+class EchoTCPServer(socketserver.TCPServer):
+    allow_reuse_address = True
 
-    hostname, addr, port = 'localhost', IPv4Address('127.0.0.1'),  10000
-
-    class RequestHandler(socketserver.BaseRequestHandler):
+    class EchoRequestHandler(socketserver.BaseRequestHandler):
         def handle(self):
-            data = self.request.recv(1024)
+            data = self.request.recv(1500)
             self.request.sendall(data)
 
-    class TCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-        allow_reuse_address = True
+    def __init__(self, server_address: Tuple[str, int]):
+        super().__init__(server_address, self.EchoRequestHandler)
+        self._server_thread: Optional[threading.Thread] = None
 
-    class SSLServer(TCPServer):
-        def get_request(self):
-            socket, addr = super().get_request()
-            ssl_socket = ssl.wrap_socket(
-                socket,
-                server_side=True,
-                keyfile=resource_dir / 'ssl.key',
-                certfile=resource_dir / 'ssl.cert',
-            )
-            return ssl_socket, addr
+    def start(self) -> None:
+        self._server_thread = threading.Thread(target=self.serve_forever)
+        self._server_thread.daemon = True
+        self._server_thread.start()
 
-    Server = SSLServer if user_ssl else TCPServer
-
-    server = Server((hostname, port), RequestHandler)
-    with server:
-        server_thread = threading.Thread(target=server.serve_forever)
-        server_thread.daemon = True
-        server_thread.start()
-        yield hostname, addr, port
-        server.shutdown()
+    def stop(self) -> None:
+        self.server_close()
+        self.shutdown()
+        if (server_thread := self._server_thread) is not None:
+            server_thread.join()
 
 
-@pytest.mark.parametrize('tcp_server', [{'use_ssl': False}], indirect=['tcp_server'])
-def test_tcp_socket_manager(tcp_server: Tuple[str, IPv4Address, int]):
-    hostname, addr, port = tcp_server
+class EchoSSLServer(EchoTCPServer):
+    def __init__(self, server_address: Tuple[str, int], keyfile: Path, certfile: Path):
+        super().__init__(server_address)
+        self._keyfile = keyfile
+        self._certfile = certfile
+
+    def get_request(self):
+        sock, addr = super().get_request()
+        ssl_socket = ssl.wrap_socket(sock, server_side=True, keyfile=self._keyfile, certfile=self._certfile)
+        return ssl_socket, addr
+
+
+@pytest.fixture(scope='module')
+def tcp_server(port_gen: Generator[int, None, None]) -> Generator[Tuple[IPv4Address, int], None, None]:
+    addr, port = IPv4Address('127.0.0.1'), next(port_gen)
+
+    server = EchoTCPServer((str(addr), port))
+    server.start()
+    yield addr, port
+    server.stop()
+
+
+@pytest.fixture(scope='module')
+def ssl_server(resource_dir: Path, port_gen: Generator[int, None, None]) -> Generator[Tuple[str, int], None, None]:
+    hostname, port = 'localhost', next(port_gen)
+
+    server = EchoSSLServer((hostname, port), keyfile=resource_dir / 'ssl.key', certfile=resource_dir / 'ssl.cert')
+    server.start()
+    yield hostname, port
+    server.stop()
+
+
+def test_tcp_socket_manager(tcp_server: Tuple[IPv4Address, int]):
+    addr, port = tcp_server
 
     pool = ConnectionPool(TcpSocketConnectionManager())
     with pool.connection((addr, port)) as sock:
@@ -63,9 +81,8 @@ def test_tcp_socket_manager(tcp_server: Tuple[str, IPv4Address, int]):
     pool.close()
 
 
-@pytest.mark.parametrize('tcp_server', [{'use_ssl': True}], indirect=['tcp_server'])
-def test_ssl_socket_manager(resource_dir: Path, tcp_server: Tuple[str, IPv4Address, int]):
-    hostname, addr, port = tcp_server
+def test_ssl_socket_manager(resource_dir: Path, ssl_server: Tuple[str, int]):
+    hostname, port = ssl_server
     ssl_context = ssl.create_default_context(cafile=resource_dir / 'ssl.cert')
 
     pool = ConnectionPool(SslSocketConnectionManager(ssl_context))
@@ -76,3 +93,31 @@ def test_ssl_socket_manager(resource_dir: Path, tcp_server: Tuple[str, IPv4Addre
         assert response == request
 
     pool.close()
+
+
+def test_tcp_socket_manager_timeout(sleep_delay, port_gen: Generator[int, None, None]):
+    addr, port = IPv4Address('127.0.0.1'), next(port_gen)
+
+    server_sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    server_sock.bind((str(addr), port))
+
+    pool = ConnectionPool(TcpSocketConnectionManager())
+    with pytest.raises(TimeoutError):
+        pool.acquire(endpoint=(addr, port), timeout=sleep_delay)
+
+    pool.close()
+    server_sock.close()
+
+
+def test_ssl_socket_manager_timeout(sleep_delay, port_gen: Generator[int, None, None]):
+    hostname, port = 'localhost', next(port_gen)
+
+    server_sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    server_sock.bind((hostname, port))
+
+    pool = ConnectionPool(SslSocketConnectionManager(ssl.create_default_context()))
+    with pytest.raises(TimeoutError):
+        pool.acquire(endpoint=(hostname, port), timeout=sleep_delay)
+
+    pool.close()
+    server_sock.close()
